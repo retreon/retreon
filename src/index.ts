ce51f19031d784371e36e6d32e999201094b4845
Jesse Gibson <JesseTheGibson@gmail.com>
2019-12-19 23:58:13 -0700

    Export `nothing` from package
    The assumption is that users won't need `immer`, and thus won't have
    access to the special `nothing` value. Now they do.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export { nothing } from 'immer';
+
 export { default as createReducer } from './create-reducer';
 export { default as createAction } from './create-action';
 export { failure } from './action-failure';
