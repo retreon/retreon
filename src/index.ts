6ec5bc4eefaebccaed9241fd44a9a99242b3cc82
Jesse Gibson <JesseTheGibson@gmail.com>
2019-12-21 15:13:52 -0700

    Add redux middleware for async iterators
    The middleware allows you to dispatch an async iterator. Every value of
    `yield` is dispatched, and the return value is returned by
    `dispatch(...)` in a promise. It's magical.
    
    My plan is to add support for async action creators, optimistic actions,
    and action sequences.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { nothing } from 'immer';
-
-export { default as createReducer } from './create-reducer';
 export { default as createAction } from './create-action';
+export { default as createReducer } from './create-reducer';
+export { default as middleware } from './redux-middleware';
 export { failure } from './action-failure';
+export { nothing } from 'immer';
---
+++ src/redux-middleware.ts
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, no-constant-condition */
+import { Middleware, AnyAction } from 'redux';
+
+/**
+ * A redux middleware which adds support for async actions and action
+ * sequences.
+ */
+const reduxMiddleware: Middleware = ({ dispatch }) => next => <
+  PossiblyAnything extends DispatchCompatible<typeof dispatch>,
+  YieldedAction extends DispatchCompatible<typeof dispatch>
+>(
+  action: PossiblyAnything | AsyncIterator<YieldedAction>,
+) => {
+  if (isIterator(action) && supportsAsyncIteration(action)) {
+    return consumeAsyncIterator(dispatch, action);
+  }
+
+  return next(action);
+};
+
+// This produces the set of all things which can be dispatched.
+type DispatchCompatible<Dispatch extends (...args: any) => any> = Parameters<
+  Dispatch
+>[0];
+
+// Consume the async iterator. Every yielded value is dispatched and the
+// return value is resolved and returned from `dispatch(...)`.
+const consumeAsyncIterator = async <
+  Dispatch extends (...args: any) => any,
+  Action extends DispatchCompatible<Dispatch>,
+  ReturnType
+>(
+  dispatch: Dispatch,
+  generator: AsyncIterator<Action, ReturnType>,
+): Promise<ReturnType> => {
+  while (true) {
+    // TODO: Decide what, if anything, should go into `.next(...)`.
+    const result: IteratorResult<Action, ReturnType> = await generator.next();
+
+    // The iterator finished. Resolve with the return value.
+    if (result.done) return result.value;
+
+    // The iterator yielded a value. Dispatch it.
+    await dispatch(result.value);
+  }
+};
+
+const supportsAsyncIteration = <T>(
+  generator: AsyncIterator<T> | Iterator<T>,
+): generator is AsyncIterator<T> => {
+  return Symbol.asyncIterator in generator;
+};
+
+const isIterator = <T>(value: any): value is AsyncIterator<T> | Iterator<T> => {
+  return (
+    value &&
+    typeof value.next === 'function' &&
+    typeof value.throw === 'function' &&
+    typeof value.return === 'function'
+  );
+};
+
+// Pull the return type out of an async iterator.
+type IteratorReturnType<
+  Iterator extends AsyncIterator<any, any, any>
+> = Iterator extends AsyncIterator<any, infer TReturn, any> ? TReturn : never;
+
+// Overload `store.dispatch(...)` to add support for async iterators.
+declare module 'redux' {
+  interface Dispatch<A extends Action = AnyAction> {
+    <Iterator extends AsyncIterator<any>>(action: Iterator): Promise<
+      IteratorReturnType<Iterator>
+    >;
+  }
+}
+
+export default reduxMiddleware;
