cee89ad2bac77ef00cbde8dec3943209a770e8ad
Jesse Gibson <JesseTheGibson@gmail.com>
2020-11-12 20:50:25 -0800

    Export error class mixin
    I've named it `swallow` because that's the intended behavior. If you've
    never used the framework before, `swallow(Error)` gives you enough to
    guess at its purpose.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as createAction } from './actions/create-action';
 export { default as createReducer } from './reducers/create-reducer';
 export { default as middleware } from './middleware/async-generator-middleware';
+export { mixin as swallow } from './utils/errors';
 export { nothing } from 'immer';
