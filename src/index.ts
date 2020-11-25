38f8e064aff9fef83568d033072b0f3f637ddd81
Jesse Gibson <JesseTheGibson@gmail.com>
2020-11-25 11:17:51 -0800

    Remove error class mixin
    This is obsolete. See ADR-0002.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { default as createAction } from './actions/create-action';
 export { default as createReducer } from './reducers/create-reducer';
 export { default as middleware } from './middleware/generator-middleware';
-export { mixin as swallow } from './utils/errors';
 export { nothing } from 'immer';
